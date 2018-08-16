8f483b1d155b2751fba6a67972ae89f36ca5b28f
Giovanni Leon <Giovanni.Leon@endava.com>
2018-08-15 22:32:59 -0500

    remove unused class comparator

--- src/main/java/com/almundo/callcenter/domain/ComparateEmployeeType.java
+++
@@ -1,8 +0,0 @@
-package com.almundo.callcenter.domain;
-
-public class ComparateEmployeeType implements Comparable<Employee>{
-    @Override
-    public int compareTo(Employee first) {
-        return 0;
-    }
-}
